42e34d292c0a07708772f77f3cdcc4f854d65946
seananigan <sdales@sfu.ca>
2020-03-01 18:46:59 -0800

    craving

--- app/server.py
+++ app/server.py
@@ -46,25 +46,38 @@ def move():
     data = bottle.request.json
     print("MOVE:", json.dumps(data))
 
-    # Choose a random direction to move in
     directions = ["up", "down", "left", "right"]
     currentLocation = data["you"]["body"][0]
     print("\n\n\n\n this is the current location: ", currentLocation, "\n\n\n")
+
+    #wall locations
+    xWalls = [-1]
+    xWalls.append(data["board"]["width"])
+    yWalls = [-1]
+    yWalls.append(data["board"]["height"])
+
+    print("\n\n xWalls: ", xWalls, "\n\n")
+    print("\n\n yWalls: ", yWalls, "\n\n")
+
     newLocations = {}
     for direction in directions:
         newLocations[direction] = {}
+    
+    #determining where each move ends up
     newLocations["up"]["x"] = currentLocation["x"]
-    newLocations["up"]["y"] = currentLocation["y"]+1
+    newLocations["up"]["y"] = currentLocation["y"]-1
     newLocations["down"]["x"] = currentLocation["x"]
-    newLocations["down"]["y"] = currentLocation["y"]-1
+    newLocations["down"]["y"] = currentLocation["y"]+1
     newLocations["left"]["x"] = currentLocation["x"]-1
     newLocations["left"]["y"] = currentLocation["y"]
-    newLocations["up"]["x"] = currentLocation["x"]+1
-    newLocations["up"]["y"] = currentLocation["y"]
+    newLocations["right"]["x"] = currentLocation["x"]+1
+    newLocations["right"]["y"] = currentLocation["y"]
 
+    #choosing a move that doesn't kill us
     counter = 0
-    for location in newLocations:
-        if location not in currentLocation != data["you"]["body"]:
+    for location in newLocations.values():
+        print("\n\n location: ", location, "\n\n")
+        if location not in data["you"]["body"] and location['x'] not in xWalls and location['y'] not in yWalls:
             move = directions[counter]
             break
         counter += 1
