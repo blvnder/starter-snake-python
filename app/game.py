c9bd811e2542fa198ee5c0aa2894c75b3688d4d2
blvnder <alicelinlc@gmail.com>
2020-03-10 21:52:26 -0700

    Set up functions

--- app/game.py
+++ app/game.py
@@ -1,53 +1,87 @@
+import utils
+
 class Board():
-    def __init__(self, height, width, food, snakes):
-        self.height = height
-        self.width = width
-        self.food = food
-        self.snakes = snakes
+    def __init__(self, data):
+        self.height = data['board']['height']
+        self.width = data['board']['width']
+        self.food = data['board']['food']
+        self.snakes = self.getSnakes(data)
+        self.player = Snake(data['you'])
+    
+    # Returns a list of enemy snake objects
+    def getSnakes(self, data):
+        enemies = []
+        for snake in data['board']['snakes']:
+            s = Snake(snake)
+            if s.id != data['you']['id']:
+                enemies.append(s)
+        return enemies
     
     # Returns a list of cells occupied by enemy snakes
-    def getSnakeSpots(self, data):
+    def getSnakeSpots(self):
         snakeSpots = []
-        if (len(self.snakes) > 0):
-            snakeList = self.snakes
-            for snake in snakeList:
-                snakeSpots += snake["body"]
+        if len(self.snakes) > 0:
+            for snake in self.snakes:
+                snakeSpots += snake.body
         return snakeSpots
-
-class Snake():
-    def __init__(self, id, name, health, body):
-        self.id = id
-        self.name = name
-        self.health = health
-        self.body = body
-        self.head = self.body[0]
-        self.tail = self.body[-1]
-
-class Player(Snake):
-    def __init__(self, id, name, health, body):
-        super().__init__(id, name, health, body)
-
-    def getSafeMoves(self, position, directions, xWalls, yWalls, snakeSpots):
-    #returns moves that go into an open space
-        safeMoves = []
-        counter = 0
-        for location in self.getNewLocations(position, directions).values():
-            if location not in self.body[:-1] and location['x'] not in xWalls and location['y'] not in yWalls and location not in snakeSpots:
-                safeMoves.append(directions[counter])
-            counter += 1
-        return safeMoves
-
-    def getNewLocations(self, position, directions):
-    #returns a dictionary of the 4 possible moves as keys with the position they lead to as values
+    
+    
+    def isValid(self, location):
+        if location['x'] < 0 or location['x'] > self.width-1 or location['y'] < 0 or location['y'] > self.height-1:
+            return False
+        snakeSpots = self.getSnakeSpots()
+        if location in snakeSpots:
+            return False
+        if location in self.player.body[:-1]:
+            return False
+        else: 
+            return True
+    
+    def getNewLocations(self, location, directions):
         newLocations = {}
         for direction in directions:
             newLocations[direction] = {}
-        newLocations["up"]["x"] = position["x"]
-        newLocations["up"]["y"] = position["y"]-1
-        newLocations["down"]["x"] = position["x"]
-        newLocations["down"]["y"] = position["y"]+1
-        newLocations["left"]["x"] = position["x"]-1
-        newLocations["left"]["y"] = position["y"]
-        newLocations["right"]["x"] = position["x"]+1
-        newLocations["right"]["y"] = position["y"]
+        newLocations["up"]["x"] = location["x"]
+        newLocations["up"]["y"] = location["y"]-1
+        newLocations["down"]["x"] = location["x"]
+        newLocations["down"]["y"] = location["y"]+1
+        newLocations["left"]["x"] = location["x"]-1
+        newLocations["left"]["y"] = location["y"]
+        newLocations["right"]["x"] = location["x"]+1
+        newLocations["right"]["y"] = location["y"]
         return newLocations
+
+    def getLegalMoves(self, location, directions):
+        legalMoves = []
+        moves = self.getNewLocations(location, directions)
+        for key, value in moves.items():
+            if self.isValid(value) == False:
+                continue
+            legalMoves.append(key)
+        return legalMoves
+            
+
+    def isWin(self):
+        for snake in self.snakes:
+            if snake.head == self.player.head:
+                if len(snake.body) < len(self.player.body):
+                    return True
+        return False
+
+    def isLose(self):
+        for snake in self.snakes:
+            if snake.head == self.player.head:
+                if len(snake.body) > len(self.player.body):
+                    return True
+        if self.player.health == 0:
+            return True
+        return False
+
+class Snake():
+    def __init__(self, data):
+        self.id = data['id']
+        self.name = data['name']
+        self.health = data['health']
+        self.body = data['body']
+        self.head = self.body[0]
+        self.tail = self.body[-1]
--- app/server.py
+++ app/server.py
@@ -47,75 +47,14 @@ def move():
     data = bottle.request.json
     print("MOVE:", json.dumps(data))
 
-    def GetSafeMoves(position, xWalls, yWalls, snakeSpots):
-    #returns moves that go into an open space
-        safeMoves = []
-        counter = 0
-        for location in GetNewLocations(position).values():
-            if location not in player.body[:-1] and location['x'] not in xWalls and location['y'] not in yWalls and location not in snakeSpots:
-                safeMoves.append(directions[counter])
-            counter += 1
-        return safeMoves
-
-    def GetNewLocations(location):
-    #returns a dictionary of the 4 possible moves as keys with the position they lead to as values
-        newLocations = {}
-        directions = ["up", "down", "left", "right"]
-        for direction in directions:
-            newLocations[direction] = {}
-        newLocations["up"]["x"] = location["x"]
-        newLocations["up"]["y"] = location["y"]-1
-        newLocations["down"]["x"] = location["x"]
-        newLocations["down"]["y"] = location["y"]+1
-        newLocations["left"]["x"] = location["x"]-1
-        newLocations["left"]["y"] = location["y"]
-        newLocations["right"]["x"] = location["x"]+1
-        newLocations["right"]["y"] = location["y"]
-        return newLocations
-    
-    def HasFood(location):
-        if location in board.food:
-            return True
-        return False
-
     directions = ["up", "down", "left", "right"]
-    board = Board(data['board']['height'], data['board']['width'], data['board']['food'], data['board']['snakes'])
-    player = Player(data['you']['id'], data['you']['name'], data['you']['health'], data['you']['body'])
+    board = Board(data)
 
-    #wall locations
-    xWalls = [-1, board.width]
-    yWalls = [-1, board.height]
-
-    snakeSpots = board.getSnakeSpots(data)
-
-    #choosing a move that doesn't kill us
-    moves = player.getSafeMoves(player.head, directions, xWalls, yWalls, snakeSpots)
-    mostDirectionsOpen = 0 #we want to choose the move that gives us the most options from there so we don't corner ourself
-    goodMoves = []
-    for option in moves:
-    #this loop looks one move into the future and chooses the moves that leave the most spaces open to move into next
-        directionsOpen = len(player.getSafeMoves(player.getNewLocations(player.head, directions)[option], directions, xWalls, yWalls, snakeSpots))
-        # print("\n directions open for ", option, " at ", GetNewLocations(currentLocation)[option], " is ", directionsOpen)
-        if directionsOpen == mostDirectionsOpen:
-            goodMoves.append(option)
-        elif directionsOpen > mostDirectionsOpen:
-            goodMoves = [option]
-            mostDirectionsOpen = directionsOpen
-    
-    if player.health < 40:
-    #prioritize finding food if all else is equal
-        foodMoves = []
-        for option in moves:
-            if HasFood(GetNewLocations(currentLocation)[option]):
-                foodMoves.append(option)
-        if len(foodMoves) > 0:
-            goodMoves = foodMoves
-    print("\n good moves: ", goodMoves)
-    # for key in directions:
+    moves = board.getLegalMoves(board.player.head, directions)
     if len(moves) == 0:
         move = random.choice(directions)
     else:
-        move = random.choice(goodMoves)
+        move = random.choice(moves)
 
     # Shouts are messages sent to all the other snakes in the game.
     # Shouts are not displayed on the game board.
